406a594b93eb0d8fa18584f5a80250e8bdf442cc
kstanley <kstanley21@yahoo.com>
2016-12-05 08:34:49 -0800

    dropdown edited

--- public/javascripts/JsonTest.js
+++ public/javascripts/JsonTest.js
@@ -1,5 +1,5 @@
-angular.module("jsonCreator", [])
-	.controller("mainCtrl", function ($scope) {
+angular.module("jsonCreator", ['inputDropdown'])
+	.controller("mainCtrl", ['$scope', '$q', function ($scope, $q) {
 		console.log("I am the main controller!");
 		var myObj = {
 			categories: {
@@ -42,6 +42,84 @@ angular.module("jsonCreator", [])
 
 			}
 		};
+		// dropdown test
+		var self = this;
+
+		self.countryString = []; // Holds the selected in demoFormStrings, set with attribute 'selected-item'
+
+		// Pass strings to the dropdown for simple usage
+		self.defaultDropdownStrings = [
+          ['China',
+          'Sweden',
+          'United Kingdom',
+          'United States'
+        ], ['test']];
+
+
+		// Filter method is passed with attribute 'filter-list-method="method(userInput)"'.
+		// Called on the onchange event from the input field. Should return a promise resolving with an array of items to show in the dropdown.
+		// If no filter method is passed to the the directive, the default dropdown will show constantly.
+		self.filterStringList = function (userInput, position) {
+			//lastUserStringInput = userInput;lastUserStringInput
+			self.countryString[position] = userInput;
+			var filter = $q.defer();
+			var normalisedInput = userInput.toLowerCase();
+
+
+			var filteredArray = self.defaultDropdownStrings[position].filter(function (country) {
+				return country.toLowerCase().indexOf(normalisedInput) === 0;
+			});
+
+			filter.resolve(filteredArray);
+			return filter.promise;
+		};
+
+		self.filterObjectList = function (userInput) {
+			var filter = $q.defer();
+			var normalisedInput = userInput.toLowerCase();
+			var filteredArray = self.defaultDropdownObjects.filter(function (country) {
+				var matchCountryName = country.readableName.toLowerCase().indexOf(normalisedInput) === 0;
+				var matchCountryCode = country.countryCode.toLowerCase().indexOf(normalisedInput) === 0;
+				return matchCountryName || matchCountryCode;
+			});
+
+			filter.resolve(filteredArray);
+			return filter.promise;
+		};
+
+
+		// Called when user selected an item from dropdown. Passed with attribute 'item-selected-method="method(item)"'.
+		self.itemStringSelected = function (item) {
+			console.log('Handle item string selected in controller:', item);
+			self.stringMessage = 'String item selected: ' + item;
+		};
+
+		self.itemObjectSelected = function (item) {
+			console.log('Handle item object selected in controller:', item);
+			self.objectMessage = 'Object item selected: ' + item;
+		};
+
+
+		self.submitFormStrings = function () {
+			if ($scope.demoFormStrings.$valid) {
+				// If the user has selected something from the dropdown, the value will be stored in self.countryString.
+				// If they haven't and we set allowCustomInput=true, we can grab the input value
+				// from lastUserStringInput which we set in filterStringList().
+				// There are probably better ways to grab the current value from the input field, this is just a quick example.
+				var country = self.countryString || lastUserStringInput;
+				console.log('Submit form STRINGS with country:', country);
+			}
+		};
+
+		self.submitFormObjects = function () {
+			if ($scope.demoFormObjects.$valid) {
+				console.log('Submit form OBJECTS with country:', self.countryObject);
+				self.objectMessage = 'Submit form OBJECT with country: ' + self.countryObject;
+			}
+		};
+
+
+		// end dropdown test
 		// first category array
 		var category = ['var1', 'var2', 'var3'];
 		// subcategory array
@@ -60,15 +138,10 @@ angular.module("jsonCreator", [])
 		// array to detirmin were to access information from
 		var query = ['categories'];
 
-
 		// change querry based on dropdown selected
 		$scope.categoryChange = function (cat) {
-			if (cat === 1) {
 
-			} else if (cat === 2) {
-
-			}
-		}
+		};
 
 		// function to trim the querry back down to size.
 		function setQuery(desiredLength) {
@@ -89,8 +162,12 @@ angular.module("jsonCreator", [])
 			}
 		}
 		getProperty(myObj.categories, category);
+		// test code for dropdown
+		// end test code for dropdown
+
 		// end controller
-	})
+	}])
+
 
 
 /*
@@ -100,7 +177,8 @@ angular.module("jsonCreator", [])
 
 	ng-class to change the class of an element
 
-
+for input dropdown problems
+	https://github.com/hannaholl/angular-input-dropdown#3-create-a-controller-to-handle-the-data-for-the-input
 
 ng-model
 */
